bdd8affd21925e99a9958882caa53b5b67908cc2
abdulrahmanibrahimdawood <abdoibrahim9111@gmail.com>
2025-02-12 13:35:29 +0200

    custom app bar

---
+++ lib/core/widgets/custom_app_bar.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+AppBar buildAppBar(context, {required String title}) {
+  return AppBar(
+    centerTitle: true,
+    title: Text(
+      title,
+      style: const TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+    ),
+    leading: GestureDetector(
+      onTap: () {
+        Navigator.pop(context);
+      },
+      child: const Icon(
+        Icons.arrow_back_ios_new,
+        color: Colors.black,
+      ),
+    ),
+  );
+}
--- lib/features/auth/presentation/views/login_view.dart
+++ lib/features/auth/presentation/views/login_view.dart
@@ -1,10 +1,19 @@
+import 'package:e_commerce/core/widgets/custom_app_bar.dart';
+import 'package:e_commerce/features/auth/presentation/views/widgets/login_view_body.dart';
 import 'package:flutter/material.dart';
 
 class LoginView extends StatelessWidget {
   const LoginView({super.key});
   static const routeName = 'login';
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Directionality(
+      textDirection: TextDirection.rtl,
+      child: Scaffold(
+        appBar: buildAppBar(context, title: 'Login'),
+        body: const LoginViewBody(),
+      ),
+    );
   }
 }
